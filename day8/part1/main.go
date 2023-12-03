5db267d0ebd2d399e47699ea855fe76c2563dc0e
ftambara <98750668+ftambara@users.noreply.github.com>
2023-12-03 16:05:09 -0300

    Add ability to count following a sequence

--- day8/part1/main.go
+++ day8/part1/main.go
@@ -11,17 +11,44 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+	totalVisible := 0
 	for _, row := range forest {
-		fmt.Printf("%v: visible (left to right): %v\n", row, visible(row))
+		// Count left to right
+		count := visible(row, genSequence(0, len(row)))
+		fmt.Printf("%v: visible (left to right): %v\n", row, count)
+		totalVisible += count
+		fmt.Println()
+		// Count right to left
+		count = visible(row, genSequence(len(row)-1, -1))
+		fmt.Printf("%v: visible (right to left): %v\n", row, count)
+		totalVisible += count
+		fmt.Println()
 	}
 }
 
-func visible(row []int) int {
-	highest := row[0]
+func genSequence(start, end int) []int {
+	var step int
+	switch {
+	case start < end:
+		step = 1
+	case start > end:
+		step = -1
+	default:
+		return nil
+	}
+	seq := make([]int, (end-start)/step)
+	for i := 0; i < len(seq); i++ {
+		seq[i] = start + i*step
+	}
+	return seq
+}
+
+func visible(row, sequence []int) int {
+	highest := row[sequence[0]]
 	count := 1 // First one counts
-	for _, tree := range row {
-		if tree > highest {
-			highest = tree
+	for _, i := range sequence {
+		if row[i] > highest {
+			highest = row[i]
 			count++
 		}
 	}
