dc9b4d9da2c0b9add585ddbcf7aff5ca977f9708
ftambara <98750668+ftambara@users.noreply.github.com>
2024-02-13 08:30:47 -0300

    Add day 12 part 2

---
+++ day12/part2/main.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	heightMap, _, end := readHeightMap("day12/input.txt")
+
+	var shortestPath []Position
+	for x := 0; x < len(heightMap[0]); x++ {
+		for y := 0; y < len(heightMap); y++ {
+			if heightMap[y][x] == 0 {
+				path := findPath(heightMap, Position{x, y}, end)
+				if path == nil {
+					fmt.Printf("No path found for x: %d, y: %d\n", x, y)
+					continue
+				}
+				fmt.Printf("New path length: %d\n", len(path))
+				if shortestPath == nil || len(path) < len(shortestPath) {
+					shortestPath = path
+				}
+			}
+		}
+	}
+	fmt.Printf("Shortest path steps: %d\n", len(shortestPath)-1)
+}
+
+func decodeHeight(height byte) (h int, start bool, end bool) {
+	switch height {
+	case 'S':
+		return 0, true, false
+	case 'E':
+		return 'z' - 'a', false, true
+	default:
+		return int(height - 'a'), false, false
+	}
+}
+
+type Position struct {
+	x, y int
+}
+
+func readHeightMap(filename string) (heightMap [][]int, start, end Position) {
+	f, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		var row []int
+		for _, height := range scanner.Text() {
+			value, isStart, isEnd := decodeHeight(byte(height))
+			row = append(row, value)
+			if isStart {
+				start = Position{len(row) - 1, len(heightMap)}
+			} else if isEnd {
+				end = Position{len(row) - 1, len(heightMap)}
+			}
+		}
+		heightMap = append(heightMap, row)
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return heightMap, start, end
+}
+
+func findPath(heightMap [][]int, start, end Position) []Position {
+	shortestPaths := map[Position][]Position{
+		start: {start},
+	}
+	positionsQueue := []Position{start}
+	for i := 0; i < len(positionsQueue); i++ {
+		position := positionsQueue[i]
+		for dx := -1; dx <= 1; dx++ {
+			for dy := -1; dy <= 1; dy++ {
+				// Skip self and diagonals
+				if (dx == 0 && dy == 0) || (dx != 0 && dy != 0) {
+					continue
+				}
+				x, y := position.x+dx, position.y+dy
+				// Skip out of bounds
+				if x < 0 || x >= len(heightMap[0]) || y < 0 || y >= len(heightMap) {
+					continue
+				}
+				// Skip if the height difference is more than 1
+				if heightMap[y][x]-heightMap[position.y][position.x] > 1 {
+					continue
+				}
+				// Skip if a shorter path already exists
+				if _, ok := shortestPaths[Position{x, y}]; ok {
+					continue
+				}
+				shortestPaths[Position{x, y}] = append(shortestPaths[position], Position{x, y})
+
+				// If we reached the destination, we can stop
+				if x == end.x && y == end.y {
+					return shortestPaths[end]
+				}
+
+				positionsQueue = append(positionsQueue, Position{x, y})
+			}
+		}
+	}
+
+	// If we reached this point, there is no path
+	return nil
+}
